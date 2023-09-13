1801d2f054e4dc625c2e16af3f23ca154dbb2954
Nuria <nuriaoujo.n1@gmail.com>
2023-09-13 12:14:20 +0200

    DietApp con mejora en addDietToPatient

--- src/main/java/com/campusdual/Ejercicio3JuanjoV/Patients/Patients.java
+++ src/main/java/com/campusdual/Ejercicio3JuanjoV/Patients/Patients.java
@@ -3,15 +3,13 @@ package com.campusdual.Ejercicio3JuanjoV.Patients;
 import com.campusdual.Ejercicio3JuanjoV.Diet;
 
 import java.sql.SQLOutput;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Patients {
-    List <Patients> patientsList = new ArrayList<>();
-    List <Diet> weeklyDiet;
-    Scanner scanner =  new Scanner(System.in);
-    Scanner scannerName =  new Scanner(System.in);
+    List<Patients> patientsList = new ArrayList<>();
+    Map<Integer, Diet> weeklyDiet;
+    Scanner scanner = new Scanner(System.in);
+    Scanner scannerName = new Scanner(System.in);
     private boolean requestDeclined;
 
     private String name;
@@ -21,16 +19,24 @@ public class Patients {
     private Integer age;
     private String genre;
     private String newGenrePatient;
+    private String day;
+    private Diet dietName;
 
-    public String getName() { return name; }
-    public String getLastName() { return lastName; }
 
-    public Patients(){
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Patients() {
         patientsList.add(new Patients("María", "Álvarez", 67, 168, 43, "Mujer", null));
         patientsList.add(new Patients("Joaquín", "Míguez", 87, 173, 27, "Hombre", null));
     }
 
-    public Patients(String name, String lastName, Integer weight, Integer height, Integer age, String genre, List weeklyDiet) {
+    public Patients(String name, String lastName, Integer weight, Integer height, Integer age, String genre, Map weeklyDiet) {
         this.name = name;
         this.lastName = lastName;
         this.weight = weight;
@@ -58,14 +64,14 @@ public class Patients {
         System.out.println("Escribe la edad del paciente:");
         Integer newAgePatient = scanner.nextInt();
 
-        while(requestDeclined) {
+        while (requestDeclined) {
             System.out.println("Proporciona el género del paciente:");
             System.out.println("Mujer(1) , Hombre(2)");
             Integer isWomenPatient = scanner.nextInt();
-            if(isWomenPatient == 1) {
+            if (isWomenPatient == 1) {
                 newGenrePatient = "Mujer";
                 requestDeclined = false;
-            } else if(isWomenPatient == 2) {
+            } else if (isWomenPatient == 2) {
                 newGenrePatient = "Hombre";
                 requestDeclined = false;
             } else {
@@ -90,55 +96,105 @@ public class Patients {
         System.out.println("======================================");
         System.out.println("Estos son tus pacientes registrados:");
 
-        if(patientsList.size() == 0) {
+        if (patientsList.size() == 0) {
             System.out.println("ERROR: Todavía no tienes pacientes registrados");
         } else {
             for (int i = 0; i < patientsList.size(); i++) {
-                System.out.println((i+1) + "." + patientsList.get(i).getName() + " " + patientsList.get(i).getLastName());
+                System.out.println((i + 1) + "." + patientsList.get(i).getName() + " " + patientsList.get(i).getLastName());
             }
             System.out.println("Selecciona al paciente del que necesitas más información: ");
             int patientSelected = scanner.nextInt();
 
             System.out.println("INFORMACIÓN SOBRE EL PACIENTE");
-            System.out.println(patientsList.get(patientSelected-1).getName() + " " + patientsList.get(patientSelected-1).getLastName());
+            System.out.println(patientsList.get(patientSelected - 1).getName() + " " + patientsList.get(patientSelected - 1).getLastName());
             System.out.println("======================================");
-            if(weeklyDiet == null) {
-                System.out.println("Todavía no existen datos del paciente " + patientsList.get(patientSelected-1).getName());
+            if (weeklyDiet == null) {
+                System.out.println("Todavía no existen datos del paciente " + patientsList.get(patientSelected - 1).getName());
             } else {
-                System.out.println("Estas son las dietas registradas para el paciente " + patientsList.get(patientSelected-1).getName());
+                System.out.println("Estas son las dietas registradas para el paciente " + patientsList.get(patientSelected - 1).getName());
             }
         }
     }
 
-    public void addDietToPatient(){
+    public void addDietToPatient() {
+        System.out.println("BIENVENIDO AL EDITOR DE DIETAS DE PACIENTES");
+        System.out.println("======================================");
+        System.out.println("Estos son tus pacientes registrados:");
 
+        if (patientsList.size() == 0) {
+            System.out.println("ERROR: Todavía no tienes pacientes registrados");
+        } else {
+            for (int i = 0; i < patientsList.size(); i++) {
+                System.out.println((i + 1) + "." + patientsList.get(i).getName() + " " + patientsList.get(i).getLastName());
+            }
+            System.out.println("Selecciona al que deseas añadirle una dieta: ");
+            int patientSelected = scanner.nextInt();
+
+            if(patientsList.get(patientSelected - 1).weeklyDiet == null) {
+                System.out.println("Todavía no has iniciado la dieta de este paciente, ¿quieres crearla? SI/NO");
+                String startNewDiet = scannerName.nextLine();
+                if(startNewDiet.equalsIgnoreCase("SI")) {
+                    weeklyDiet = new HashMap<>();
+                    weeklyDiet.put(1, null); //Lunes
+                    weeklyDiet.put(2, null); //Martes
+                    weeklyDiet.put(3, null); //Miércoles
+                    weeklyDiet.put(4, null); //Jueves
+                    weeklyDiet.put(5, null); //Viernes
+                    weeklyDiet.put(6, null); //Sábado
+                    weeklyDiet.put(7, null); //Domingo
+                    System.out.println("La dieta del paciente " + patientsList.get(patientSelected - 1).getName() + " " + patientsList.get(patientSelected - 1).getLastName() + " ha sido creada");
+                } else if (startNewDiet.equalsIgnoreCase("NO")) {
+                    System.out.println("Operación cancelada");
+                } else {
+                    System.out.println("ERROR, no se ha entendido la respuesta");
+                    addDietToPatient();
+                }
+            } else {
+                System.out.println("Selecciona la dieta que quieres añadir"); //TODO en progreso...
+            }
+
+            patientsList.remove(patientsList.get(patientSelected - 1));
+        }
     }
 
-    public void removeDietToPatient() {
+    public void removeDietToPatient () {
+        System.out.println("BIENVENIDO AL EDITOR DE DIETAS DE PACIENTES");
+        System.out.println("======================================");
+        System.out.println("Estos son tus pacientes registrados:");
 
+        if (patientsList.size() == 0) {
+            System.out.println("ERROR: Todavía no tienes pacientes registrados");
+        } else {
+            for (int i = 0; i < patientsList.size(); i++) {
+                System.out.println((i + 1) + "." + patientsList.get(i).getName() + " " + patientsList.get(i).getLastName());
+            }
+            System.out.println("Selecciona al que deseas eliminarle una dieta: ");
+            int patientSelected = scanner.nextInt();
+
+        }
     }
 
-    public void dischargePatient() {
+    public void dischargePatient () {
         System.out.println("BIENVENIDO AL PROGRAMA PARA DAR DE BAJA A PACIENTES");
         System.out.println("======================================");
         System.out.println("Estos son tus pacientes registrados:");
 
-        if(patientsList.size() == 0) {
+        if (patientsList.size() == 0) {
             System.out.println("ERROR: Todavía no tienes pacientes registrados");
         } else {
             for (int i = 0; i < patientsList.size(); i++) {
-                System.out.println((i+1) + "." + patientsList.get(i).getName() + " " + patientsList.get(i).getLastName());
+                System.out.println((i + 1) + "." + patientsList.get(i).getName() + " " + patientsList.get(i).getLastName());
             }
             System.out.println("Selecciona al paciente que deseas dar de baja: ");
             int patientSelected = scanner.nextInt();
 
             System.out.println("¿Estás seguro que deseas dar de baja a este paciente? SI/NO");
-            System.out.println(patientsList.get(patientSelected-1).getName() + " " + patientsList.get(patientSelected-1).getLastName());
+            System.out.println(patientsList.get(patientSelected - 1).getName() + " " + patientsList.get(patientSelected - 1).getLastName());
             String dischargeOption = scannerName.nextLine();
 
-            if(dischargeOption.equalsIgnoreCase("SI")) {
-                System.out.println("El paciente " + patientsList.get(patientSelected-1).getName() + " " + patientsList.get(patientSelected-1).getLastName() + " ha sido eliminado");
-                patientsList.remove(patientsList.get(patientSelected-1));
+            if (dischargeOption.equalsIgnoreCase("SI")) {
+                System.out.println("El paciente " + patientsList.get(patientSelected - 1).getName() + " " + patientsList.get(patientSelected - 1).getLastName() + " ha sido eliminado");
+                patientsList.remove(patientsList.get(patientSelected - 1));
             } else if (dischargeOption.equalsIgnoreCase("NO")) {
                 System.out.println("Operación cancelada");
             } else {
